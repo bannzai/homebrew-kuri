810b97699bc8dc9ec2ddcfde65e2cc0bc2e40409
Hirose.Yudai <hirose.yudai@moneyforward.co.jp>
2017-01-11 14:38:44 +0900

    edit install

--- kuri.rb
+++ kuri.rb
@@ -1,25 +1,25 @@
-# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
+# Documentation: http://docs.brew.sh/Formula-Cookbook.html
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class Kuri < Formula
   desc ""
   homepage ""
-  url "https://github.com/bannzai/Kuri/archive/0.1.1.tar.gz"
-  sha256 "de3e2f51ff9a3b9515567b3d3fe6e4ea876f9f4a03b7f907131a63dce9526fd4"
+  url "https://github.com/bannzai/Kuri/releases/download/0.1.1/kuri.zip"
+  sha256 "6fd315859b786c3907ada6956dbf7f7286df424c75c4123530e1aa6f2b892377"
 
   # depends_on "cmake" => :build
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
 
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+     #Remove unrecognized options if warned by configure
+    #system "./configure", "--disable-debug",
+    #                      "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
+    ## system "cmake", ".", *std_cmake_args
+    bin.install 'kuri'
   end
 
   test do
@@ -27,7 +27,7 @@ class Kuri < Formula
     #
     # This test will fail and we won't accept that! It's enough to just replace
     # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test Kuri`. Options passed
+    # were more thorough. Run the test with `brew test kuri`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any

91c5b3c8f17f1ce2dc464b767fbf8b6e23b39369
Hirose.Yudai <hirose.yudai@moneyforward.co.jp>
2017-04-18 17:12:28 +0900

    Fix kuri.zip from release

--- kuri.rb
+++ kuri.rb
@@ -4,7 +4,7 @@
 
 class Kuri < Formula
   homepage "https://github.com/bannzai/Kuri/"
-  url "https://github.com/bannzai/Kuri/blob/master/brew/kuri.zip"
+  url "https://github.com/bannzai/Kuri/releases/download/0.2.1/kuri.zip"
   sha256 "41e4d654b0c50f7e4025e0a69c6b7b7b6456502cbd097dfe84621bb501de409d"
 
   # depends_on "cmake" => :build

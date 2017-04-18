9d2084239f4850a8142691f487bfc9ed1152ab13
Hirose.Yudai <hirose.yudai@moneyforward.co.jp>
2017-04-18 17:11:16 +0900

    Fix 0.2.1

--- kuri.rb
+++ kuri.rb
@@ -4,8 +4,8 @@
 
 class Kuri < Formula
   homepage "https://github.com/bannzai/Kuri/"
-  url "https://github.com/bannzai/Kuri/releases/download/0.2.1/kuri.zip"
-  sha256 "7109434a9106666bd323dab91c0db3cba5f8419ec690037121881738d96a8ded"
+  url "https://github.com/bannzai/Kuri/blob/master/brew/kuri.zip"
+  sha256 "41e4d654b0c50f7e4025e0a69c6b7b7b6456502cbd097dfe84621bb501de409d"
 
   # depends_on "cmake" => :build
 

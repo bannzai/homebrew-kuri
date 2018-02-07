613647ea18681f810d8f1168abc09367e0398eaa
Hirose.Yudai <hirose.yudai@moneyforward.co.jp>
2018-02-07 17:52:10 +0900

    :up: 0.3.2

--- kuri.rb
+++ kuri.rb
@@ -1,32 +1,28 @@
-# Documentation: http://docs.brew.sh/Formula-Cookbook.html
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
-class Kuri < Formula
-  homepage "https://github.com/bannzai/Kuri/"
-  url "https://github.com/bannzai/Kuri/releases/download/0.3.0/kuri.zip"
-  sha256 "0cce00fbd59cf22959d7579425cb92ca3261b96b730bfc70cf053e498f988eb7"
-
-  # depends_on "cmake" => :build
-
-  def install
+ class Kuri < Formula
+  desc "Automatically generates necessary code for iOS CleanArchitecture and imports it into Xcode project."
+  homepage ""
+  url "https://github.com/bannzai/Kuri/releases/download/0.3.2/kuri.zip"
+  sha256 "f01eb47392d80b64ec467abf499460649f4f7bdc3a45f416711e66532004606d"
+   # depends_on "cmake" => :build
+   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
-
-     #Remove unrecognized options if warned by configure
-    #system "./configure", "--disable-debug",
-    #                      "--disable-dependency-tracking",
-    #                      "--disable-silent-rules",
-    #                      "--prefix=#{prefix}"
-    ## system "cmake", ".", *std_cmake_args
-    bin.install 'kuri'
+     # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
   end
-
-  test do
+   test do
     # `test do` will create, run in and delete a temporary directory.
     #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test kuri`. Options passed
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test Kuri`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any

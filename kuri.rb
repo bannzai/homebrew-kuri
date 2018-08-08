4334d85b552595722179b4ce345896f99f5829b0
bannzai <kingkong999yhirose@gmail.com>
2018-08-09 02:38:17 +0900

    :shell: brew create https://github.com/bannzai/Kuri/releases/download/0.3.3/kuri.zip

--- kuri.rb
+++ kuri.rb
@@ -1,19 +1,25 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+# Documentation: https://docs.brew.sh/Formula-Cookbook
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
- class Kuri < Formula
-  desc "Automatically generates necessary code for iOS CleanArchitecture and imports it into Xcode project."
-  homepage ""
-  url "https://github.com/bannzai/Kuri/releases/download/0.3.2/kuri.zip"
-  sha256 "f01eb47392d80b64ec467abf499460649f4f7bdc3a45f416711e66532004606d"
-   # depends_on "cmake" => :build
-   def install
+class Kuri < Formula
+  desc "Automatically generates from template and imports it into Xcode project."
+  homepage "https://github.com/bannzai/Kuri/"
+  url "https://github.com/bannzai/Kuri/releases/download/0.3.3/kuri.zip"
+  sha256 "74a6eddc3de4b84154df123f24e4d51e4963394db348937803db7f90f512bec2"
+  # depends_on "cmake" => :build
+
+  def install
     # ENV.deparallelize  # if your formula fails when building in parallel
-     # Remove unrecognized options if warned by configure
+    # Remove unrecognized options if warned by configure
+    # system "./configure", "--disable-debug",
+    #                       "--disable-dependency-tracking",
+    #                       "--disable-silent-rules",
+    #                       "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
     bin.install 'kuri'
   end
-   test do
+
+  test do
     # `test do` will create, run in and delete a temporary directory.
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
